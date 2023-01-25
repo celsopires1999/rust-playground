daf3f01357e3d1b67e75399e5b617c397eba9186
celsopires1999 <celso.pires1999@gmail.com>
2023-01-25 23:51:42 +0000

    feat: error handling on main

--- users/src/main.rs
+++ users/src/main.rs
@@ -19,19 +19,17 @@ pub struct User {
     pub role: String,
 }
 
-fn main() -> Result<(), Error> {
+fn main() {
     let users = Arc::new(initialize_users());
 
     let body = Body {
-        email: "1@my.com".to_string(),
+        email: "2@my.com".to_string(),
         pw: "123".to_string(),
     };
     let user_result = find_user(users, body);
 
     match user_result {
         Ok(user) => println!("{}", user.email),
-        Err(e) => return Err(e),
+        Err(e) => println!("Error: {}", e),
     };
-
-    Ok(())
 }
